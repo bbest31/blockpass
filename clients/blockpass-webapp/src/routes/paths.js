bcf4873bd28cfdaa0bd9ecc87619bbf3acb7866e
bbest-blockpass <brandon@blockpass.live>
2022-05-29 21:18:11 -0400

    feat: nav bar updated with proper headings

--- clients/blockpass-webapp/src/routes/paths.js
+++ clients/blockpass-webapp/src/routes/paths.js
@@ -42,6 +42,7 @@ export const PATH_DASHBOARD = {
     analytics: path(ROOTS_DASHBOARD, '/analytics'),
     banking: path(ROOTS_DASHBOARD, '/banking'),
     booking: path(ROOTS_DASHBOARD, '/booking'),
+    account: path(ROOTS_DASHBOARD, '/account'),
   },
   mail: {
     root: path(ROOTS_DASHBOARD, '/mail'),
