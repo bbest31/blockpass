6c7cc0c5f20f5f3b396c71b0c973bf87a8d5e63a
Willy Liao <willyliao0425@gmail.com>
2022-05-30 12:01:04 -0700

    feat: connected to MongoDB and created Event schema and model

--- servers/blockpass-server/index.js
+++ servers/blockpass-server/index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/Blockpass');
+}
+main().catch((err) => console.log(err));
+
+app.get('/', async (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
