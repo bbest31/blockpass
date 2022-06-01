47f7fe22552e8b86d06994e4841ecaa7425dfc85
Willy Liao <willyliao0425@gmail.com>
2022-06-01 09:52:29 -0700

    fix: removed punctuations from response messages

--- servers/blockpass-server/src/controllers/responseMessages.js
+++ servers/blockpass-server/src/controllers/responseMessages.js
@@ -2,8 +2,8 @@
 
 const httpResponseMessage = {
   401: 'Unauthorzied',
-  404: 'Not found.',
-  500: 'Internal server error.',
+  404: 'Not found',
+  500: 'Internal server error',
 };
 
 module.exports = { httpResponseMessage };
