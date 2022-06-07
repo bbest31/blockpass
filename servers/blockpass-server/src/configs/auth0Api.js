57b10aacc67e3ff886381c9b9d9f91352f739ee9
Willy Liao <willyliao0425@gmail.com>
2022-06-06 20:43:15 -0700

    refactor: prepended AUTH0_ to auth0 specific env variables for specificity

--- servers/blockpass-server/src/configs/auth0Api.js
+++ servers/blockpass-server/src/configs/auth0Api.js
@@ -1,7 +1,7 @@
 'use strict';
 const AUTH0_API = {
-  audience: process.env.AUDIENCE,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 };
 
 module.exports = AUTH0_API;
