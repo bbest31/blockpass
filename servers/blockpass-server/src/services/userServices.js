982ba5f6e1f5bb045f34c15dc1b8f53e04d3b206
bbest-blockpass <brandon@blockpass.live>
2022-06-09 13:33:28 -0400

    feat: update user endpoint finished with email verification

--- servers/blockpass-server/src/services/userServices.js
+++ servers/blockpass-server/src/services/userServices.js
@@ -4,16 +4,36 @@ const { managementAPI } = require('../apis/auth0Api.js');
 const USER_ATTRIBUTES = ['email', 'name'];
 
 async function patchUser(userId, payload) {
-  const user = { id: userId, payload: payload };
+  let updateEmail = false;
   // search for disallowed payload attributes
   Object.keys(payload).forEach((key) => {
     if (!USER_ATTRIBUTES.includes(key)) {
       throw new Error('user attribute not allowed to be updated');
+    } else if (key === 'email') {
+      updateEmail = true;
     }
   });
 
-    //TODO: if updating email send verify email address 
-  return managementAPI.updateUser({ id: userId }, payload);
+  const user = await managementAPI
+    .updateUser({ id: userId }, payload)
+    .then((userData) => {
+      // if we are updating the user email, then we need to send a verification email to them.
+      if (updateEmail) {
+        managementAPI
+          .sendEmailVerification({ user_id: userData.user_id })
+          .then(() => {
+            console.log(`Email verification sent to ${userData.email}`);
+          })
+          .catch((err) => {
+            console.error('Error sending email verification email: ', err);
+          });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+  return user;
 }
 
 module.exports = {
