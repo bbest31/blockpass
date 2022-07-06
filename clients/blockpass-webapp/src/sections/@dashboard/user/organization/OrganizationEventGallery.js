b9bb51a1ed7bbff7128cc43d199a00f97650a36a
Willy Liao <willyliao0425@gmail.com>
2022-07-06 15:31:26 -0700

    refactor: extracted the OrganizationGalleryItems from OrganizationEventGallery into its own file

--- clients/blockpass-webapp/src/sections/@dashboard/user/organization/OrganizationEventGallery.js
+++ clients/blockpass-webapp/src/sections/@dashboard/user/organization/OrganizationEventGallery.js
@@ -1,28 +1,8 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 // @mui
-import { styled } from '@mui/material/styles';
-import { Box, Card, IconButton, Typography, CardContent, InputAdornment } from '@mui/material';
-// utils
-import { fDateTimeSuffix } from '../../../../utils/formatTime';
-import cssStyles from '../../../../utils/cssStyles';
-// components
-import Image from '../../../../components/Image';
-import Iconify from '../../../../components/Iconify';
-
-// ----------------------------------------------------------------------
-
-const CaptionStyle = styled(CardContent)(({ theme }) => ({
-  ...cssStyles().bgBlur({ blur: 2, color: theme.palette.grey[900] }),
-  bottom: 0,
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  position: 'absolute',
-  justifyContent: 'space-between',
-  color: theme.palette.common.white,
-}));
-
+import { Box, Card } from '@mui/material';
+// sections
+import OrganizationGalleryItem from './OrganizationGalleryItem';
 // ----------------------------------------------------------------------
 
 OrganizationEventGallery.propTypes = {
@@ -45,39 +25,11 @@ export default function OrganizationEventGallery({ gallery, tab }) {
             },
           }}
         >
-          {gallery.length !== 0 ? gallery.map((image) => (
-            <GalleryItem key={image._id} image={image} />
+          {gallery.length !== 0 ? gallery.map((event) => (
+            <OrganizationGalleryItem key={event._id} event={event} />
           )) : `No ${tab} events`}
         </Box>
       </Card>
     </Box>
-  );
-}
-
-// ----------------------------------------------------------------------
-
-GalleryItem.propTypes = {
-  image: PropTypes.object,
-  onOpenLightbox: PropTypes.func,
-};
-
-function GalleryItem({ image }) {
-  const { img, name, startDate } = image;
-  return (
-    <Card sx={{ cursor: 'pointer', position: 'relative' }}>
-      <Image alt="gallery image" ratio="1/1" src={img} />
-
-      <CaptionStyle>
-        <div>
-          <Typography variant="subtitle1">{name}</Typography>
-          <Typography variant="body2" sx={{ opacity: 0.72 }}>
-            {fDateTimeSuffix(startDate)}
-          </Typography>
-        </div>
-        <IconButton color="inherit">
-          <Iconify icon={'eva:more-vertical-fill'} width={20} height={20} />
-        </IconButton>
-      </CaptionStyle>
-    </Card>
   );
 }
--- clients/blockpass-webapp/src/sections/@dashboard/user/organization/index.js
+++ clients/blockpass-webapp/src/sections/@dashboard/user/organization/index.js
@@ -1,3 +1,4 @@
 export { default as OrganizationEventGallery } from './OrganizationEventGallery';
 export { default as OrganizationEventToolbar } from './OrganizationEventToolbar';
 export { default as OrganizationCover } from './OrganizationCover';
+export { default as OrganizationGalleryItem } from './OrganizationGalleryItem';
