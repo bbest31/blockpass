9200304501864e004b8b9ec7c89380152dc4a711
bbest-blockpass <brandon@blockpass.live>
2023-03-24 09:48:44 -0400

    ref: added enhancements to ticket-tiers and refactored models to have validators

--- servers/blockpass-server/src/models/TicketTiers.js
+++ servers/blockpass-server/src/models/TicketTiers.js
@@ -1,18 +1,35 @@
 'use strict';
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
-const schema = new mongoose.Schema(
+const schema = new Schema(
   {
     contract: {
       type: String,
-      required: true,
+      required: [true, 'Contract address is required'],
+      validate: {
+        validator: function (v) {
+          return /^0x[a-fA-F0-9]{40}$/g.test(v);
+        },
+        message: (props) => `${props.value} is not a valid contract address!`,
+      },
     },
     description: {
       type: String,
     },
     displayName: {
       type: String,
-      required: true,
+      required: [true, 'Display name is required'],
+      validate: {
+        validator: function (v) {
+          return String(v).trim().length > 0;
+        },
+        message: (props) => `${props.value} can not be empty!`,
+      },
+    },
+    enhancements: {
+      type: [ObjectId],
     },
   },
   {
