51c16c979c71ad91c65a4aba45a2179e34011bf6
Willy Liao <willyliao0425@gmail.com>
2023-03-26 11:08:09 -0700

    refactor: removed additional unused files and imports

--- clients/blockpass-webapp/src/sections/auth/login/LoginForm.js
+++ clients/blockpass-webapp/src/sections/auth/login/LoginForm.js
@@ -1,20 +1,15 @@
 import * as Yup from 'yup';
-// import { useState } from 'react';
-// import { Link as RouterLink } from 'react-router-dom';
 // form
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 // @mui
-import { Link, Stack, Alert, IconButton, InputAdornment } from '@mui/material';
+import { Stack, Alert } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-// routes
-// import { PATH_AUTH } from '../../../routes/paths';
 // hooks
 import useAuth from '../../../hooks/useAuth';
 import useIsMountedRef from '../../../hooks/useIsMountedRef';
 // components
-import Iconify from '../../../components/Iconify';
-import { FormProvider, RHFTextField, RHFCheckbox } from '../../../components/hook-form';
+import { FormProvider } from '../../../components/hook-form';
 
 // ----------------------------------------------------------------------
 
@@ -23,20 +18,9 @@ export default function LoginForm() {
 
   const isMountedRef = useIsMountedRef();
 
-  // const [showPassword, setShowPassword] = useState(false);
+  const LoginSchema = Yup.object().shape({});
 
-  const LoginSchema = Yup.object().shape({
-    // email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-    // password: Yup.string().required('Password is required'),
-    // organization: Yup.string().required('Organization is required'),
-  });
-
-  const defaultValues = {
-    // email: '',
-    // password: '',
-    // organization: '',
-    // remember: false,
-  };
+  const defaultValues = {};
 
   const methods = useForm({
     resolver: yupResolver(LoginSchema),
@@ -50,7 +34,7 @@ export default function LoginForm() {
     formState: { errors, isSubmitting },
   } = methods;
 
-  const onSubmit = async (data) => {
+  const onSubmit = async () => {
     try {
       await login();
     } catch (error) {
@@ -68,31 +52,7 @@ export default function LoginForm() {
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={3} sx={{ my: 2 }}>
         {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
-
-        {/* <RHFTextField name="organization" label="Organization" /> */}
-
-        {/* <RHFTextField
-          name="password"
-          label="Password"
-          type={showPassword ? 'text' : 'password'}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        /> */}
       </Stack>
-
-      {/* <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
-        <RHFCheckbox name="remember" label="Remember me" /> 
-        <Link component={RouterLink} variant="subtitle2" to={PATH_AUTH.resetPassword}>
-          Forgot password?
-        </Link>
-      </Stack> */}
 
       <LoadingButton fullWidth size="large" type="submit" variant="contained" loading={isSubmitting}>
         Login
